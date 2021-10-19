2b9313c8f70b80b0b6741da8cbcb3480834a3cbc
pedrolucaspalma <pedrolucaspalma@gmail.com>
2021-10-19 11:35:15 -0300

    add routes.ts

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,11 @@
-import express from 'express'
+import express, { Request, Response, NextFunction} from "express";
+
+import { router } from "./routes";
 
 const app = express()
+
+app.use(express.json())
+
+app.use(router)
 
 const server = app.listen(3000, () => console.log("Server is running"))
