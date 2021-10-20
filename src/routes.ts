2181815e8dbb1d70be60929c8c6bfe17337bbe18
pedrolucaspalma <pedrolucaspalma@gmail.com>
2021-10-20 11:28:30 -0300

    add AuthenticateUserController. Add new controller to routes

--- src/routes.ts
+++ src/routes.ts
@@ -2,13 +2,16 @@ import { Router } from "express"
 
 import { CreateUserController } from "./controllers/CreateUserController"
 import { CreateEventController } from "./controllers/CreateEventController"
+import { AuthenticateUserController } from "./controllers/AuthenticateUserController"
 
 const router = Router()
 
-const createUserController = new CreateUserController()
-const createEventController = new CreateEventController()
+const createUserController = new CreateUserController();
+const createEventController = new CreateEventController();
+const authenticateUserController = new AuthenticateUserController();
 
 router.post("/users", createUserController.handle)
 router.post("/events", createEventController.handle)
+router.post("/login", authenticateUserController.handle)
 
 export { router }
