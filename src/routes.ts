7b547239ac00c3d73fee3c550ea7f648727dfdf1
pedrolucaspalma <pedrolucaspalma@gmail.com>
2021-10-20 15:09:03 -0300

    add authentication requirement to event creation

--- src/routes.ts
+++ src/routes.ts
@@ -4,6 +4,8 @@ import { CreateUserController } from "./controllers/CreateUserController"
 import { CreateEventController } from "./controllers/CreateEventController"
 import { AuthenticateUserController } from "./controllers/AuthenticateUserController"
 
+import { ensureAuthenticated } from "./middlewares/ensureAuthenticated"
+
 const router = Router()
 
 const createUserController = new CreateUserController();
@@ -11,7 +13,7 @@ const createEventController = new CreateEventController();
 const authenticateUserController = new AuthenticateUserController();
 
 router.post("/users", createUserController.handle)
-router.post("/events", createEventController.handle)
+router.post("/events", ensureAuthenticated, createEventController.handle)
 router.post("/login", authenticateUserController.handle)
 
 export { router }
