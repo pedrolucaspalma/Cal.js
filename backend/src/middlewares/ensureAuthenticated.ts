7dd1f60059bea0a87d7d5bce3b0d85b638fdeb21
pedrolucaspalma <pedrolucaspalma@gmail.com>
2021-10-29 15:44:14 -0300

    last commit

---
+++ backend/src/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,36 @@
+
+import { Request, Response, NextFunction } from "express";
+import {verify} from "jsonwebtoken";
+
+interface IPayload{
+    sub: string;
+}
+
+export function ensureAuthenticated(request: Request, response: Response, next: NextFunction){
+
+    const authToken = request.headers.authorization;
+
+    if(!authToken){
+        return response.status(401).end();
+    }
+
+    const [,token] = authToken.split(" ")
+
+    try{
+
+        const { sub } = verify(token ,"dfbbea8a502ceaebb95ad72d7b8866ac") as IPayload;
+
+        const convertedId = parseInt(sub)
+
+        request.userId = convertedId;
+
+        console.log(request.userId)
+
+        return next();
+
+    } catch(err){
+        return response.status(401).end();
+    }
+
+
+}
